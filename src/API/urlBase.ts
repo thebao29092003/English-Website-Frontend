703b727fd8bb6c17abe8cee3dd944c15a1f2e8e6
The Bao <thebao29092003@gmail.com>
2026-07-28 18:21:50 +0700

    thay url base dot net

--- src/API/urlBase.ts
+++ src/API/urlBase.ts
@@ -1,2 +1,3 @@
-export const URL_DOT_NET = "https://localhost:7025";
+// export const URL_DOT_NET = "https://localhost:7025";
+export const URL_DOT_NET = "https://d7792j24-7025.asse.devtunnels.ms";
 export const URL_FRONT_END = "https://localhost:5173";
