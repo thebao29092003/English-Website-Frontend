f6366d97ca9f105311a36ed6d0cf3de998f46738
The Bao <thebao29092003@gmail.com>
2026-07-28 18:25:40 +0700

    debug xong trên điện thoại

--- src/API/urlBase.ts
+++ src/API/urlBase.ts
@@ -1,3 +1,2 @@
-// export const URL_DOT_NET = "https://localhost:7025";
-export const URL_DOT_NET = "https://d7792j24-7025.asse.devtunnels.ms";
+export const URL_DOT_NET = "https://localhost:7025";
 export const URL_FRONT_END = "https://localhost:5173";
